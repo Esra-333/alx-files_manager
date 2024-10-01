6edb336d9df0cda5cc19b3b5587ca7e37df73d5d
malazhatim <malaz.hatim@gmail.com>
2024-10-01 17:50:45 +0000

    1

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -3,17 +3,35 @@ import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
 export default class AppController {
+  /**
+   * GET /status
+   * Returns the status of Redis and the database.
+   * Example response: { "redis": true, "db": true }
+   */
   static getStatus(req, res) {
+    // Check if Redis and the DB are alive, and return their statuses
     res.status(200).json({
-      redis: redisClient.isAlive(),
-      db: dbClient.isAlive(),
+      redis: redisClient.isAlive(),  // Calls isAlive() method to check Redis status
+      db: dbClient.isAlive(),        // Calls isAlive() method to check DB status
     });
   }
 
+  /**
+   * GET /stats
+   * Returns the number of users and files in the database.
+   * Example response: { "users": 12, "files": 1231 }
+   */
   static getStats(req, res) {
+    // Use Promise.all to wait for both dbClient.nbUsers() and dbClient.nbFiles()
     Promise.all([dbClient.nbUsers(), dbClient.nbFiles()])
       .then(([usersCount, filesCount]) => {
+        // Respond with a JSON object containing the number of users and files
         res.status(200).json({ users: usersCount, files: filesCount });
+      })
+      .catch((error) => {
+        // Handle any potential errors
+        res.status(500).json({ error: 'Unable to retrieve stats' });
       });
   }
 }
+
